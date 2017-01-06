61fc87057071c6c4b70b39c12f147aaf07b458b6
joe2718 <joeyjnapoleon@gmail.com>
2017-01-06 12:56:46 -0600

    sound working

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
+var sounds = []; // array of sound objects 
 
 
 $(function (){
-	// $( "#centerCircle" ).click(function() {
-	//   alert( "Handler for .click() called." );
-	// });
-	// clicks have to be here after doc loads 
+	createSounds();
+	mapBorders();
+	// directly from MIDI.js example
+
+
+
+});
+
+// *********************** This just plays sounds **************************************
+function colorClick(noteNum){  // going to keep this here just in case I want to mess with sound
+     sounds[noteNum].currentTime = 0;
+	sounds[noteNum].play();
+	console.log(noteNum);
+}
+
+
+function test(){
+	console.log("test");
+}
+// ************************* creates the sound objects *********************
+// hoping that it loads them upon creation too. 
+function createSounds(){
+	let soundLocs = ["mp3/A0.mp3", "mp3/C3.mp3","mp3/E3.mp3","mp3/G3.mp3","mp3/G4.mp3"];
+	for (index in soundLocs){
+		var audio = new Audio(soundLocs[index]);
+		sounds.push(audio);
+	}
+	console.log(sounds)
+}
+
+
+
+
+// ******************************** maps borders to sound clicks *********************
+function mapBorders(){
 	$('#colorCircle').click(function(ele){
 		// let outer=$(this).outerHeight();
 		// let inner=$(this).innerHeight();
@@ -23,74 +55,38 @@ $(function (){
 			if (y <= 0){ // Y is negative
 				if (x > y){
 					console.log('top');
-					colorClick(80);
+					colorClick(1);
 				} else {
 					console.log('left')	
-					colorClick(50);				
+					colorClick(4);				
 				}
 			} else { // y is positive
 				if (Math.abs(x) > y){
 					console.log('left');
-					colorClick(50);
+					colorClick(4);
 				} else {
 					console.log('bottom')
-					colorClick(60);
+					colorClick(3);
 				}
 			}
 		} else { // X is positive
 			if (y <= 0){ // Y is negative
 				if (x > Math.abs(y)){
 					console.log('right');
-					colorClick(70);					
+					colorClick(2);					
 				} else {
 					console.log('top')
-					colorClick(80);
+					colorClick(1);
 				}
 			} else { // y is positive
 				if (x > y){
 					console.log('right ');
-					colorClick(70);
+					colorClick(2);
 				} else {
 					console.log('bottom');
-					colorClick(60);
+					colorClick(3);
 				}
 			}
 		}
-
-
-
 	});
-	// directly from MIDI.js example
-
-
-
-});
-
-
-function colorClick(noteNum){
-	MIDI.loadPlugin({
-	soundfontUrl: "../../../lib/js/soundfont/",
-	instrument: "acoustic_grand_piano",
-	onprogress: function(state, progress) {
-		console.log(state, progress);
-	},
-	onsuccess: function() {
-		var delay = 0; // play one note every quarter second
-		var note = noteNum; // the MIDI note
-		var velocity = 127; // how hard the note hits
-		// play the note
-		MIDI.setVolume(0, 127);
-		MIDI.noteOn(0, note, velocity, delay);
-		MIDI.noteOff(0, note, delay + 0.75);
-	}
-});
 }
-
-
-
-
-function test(){
-	console.log("test");
-}
-
-// .click(selectSquare) 
